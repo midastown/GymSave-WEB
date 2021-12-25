a691a466a874201cc80a3097b5f2e6ace3539ce5
Mehdi Hachimi <mehdihachimi1@gmail.com>
2021-12-25 15:55:20 -0500

    search sessions by date complete

--- routes/sessions.js
+++ routes/sessions.js
@@ -2,9 +2,52 @@ const express = require('express')
 const router = express.Router()
 const Session = require('../models/session')
 
+// Index page
 router.get('/', (req, res) => {
     res.render('sessions/index')
 })
+
+// Search page
+router.get('/search-sessions', (req, res) => {
+    res.render('sessions/search-sessions')
+})
+
+// temp
+const getMethods = (obj) => {
+  let properties = new Set()
+  let currentObj = obj
+  do {
+    Object.getOwnPropertyNames(currentObj).map(item => properties.add(item))
+  } while ((currentObj = Object.getPrototypeOf(currentObj)))
+  return [...properties.keys()].filter(item => typeof obj[item] === 'function')
+}
+
+// Search by date
+router.get('/search-by-date', async (req, res) => {
+    let range = 1
+    let datePicked = req.query.date_picked.split('-')
+
+
+    try {
+        const sessions = await Session.find({
+            date: { // months start at 0
+                $gte: new Date(datePicked[0], datePicked[1] - 1 , datePicked[2]),
+                $lt: new Date(datePicked[0], datePicked[1] - 1, datePicked[2] + range)
+            }
+        })
+        if (sessions.toString() == "") {
+            res.json({message: "no sessions were found for the coresponding date"})
+        } else {
+            res.json(sessions)
+        }
+    } catch (e) {
+        res.status(500).json({message: e.message})
+    }
+
+    
+
+})
+
 // get all sessions
 router.get('/all-sessions', async (req, res) => {
     try{
