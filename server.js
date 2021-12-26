0c94c3efe72e97b87da670c020e5c2aded6354fd
Mehdi Hachimi <mehdihachimi1@gmail.com>
2021-12-26 16:58:50 -0500

    adding layouts for ejs

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 
+const path = require('path')
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const expressLayouts = require('express-ejs-layouts')
+const app = express()
 const port = 3000
-const path = require('path')
 
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
@@ -16,6 +17,12 @@ db.once('open', () => console.log('connected to database'))
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+// use of static files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/img'))
+
+
 
 const indexRouter = require('./routes/index')
 app.use('/', indexRouter)
@@ -24,6 +31,7 @@ const sessionRouter = require('./routes/sessions')
 app.use('/sessions', sessionRouter)
 
 app.set('views', path.join(__dirname, 'views'))
+app.use(expressLayouts)
 app.set('view engine', 'ejs')
 
 app.listen(port, () => console.log("Server OK!"))
